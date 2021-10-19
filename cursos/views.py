949a2efd79226c0794103205922aaa1e40a9ec1d
rafael.chaves <@Fonso0910>
2021-10-19 16:49:30 -0300

    criação do metodo get e urls

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    """
+        API de Curso
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    """API Avaliação"""
+    def get(self, request):
+        avaliacao = Avaliacao.objects.all()
+        serializer = CursoSerializer(avaliacao, many=True)
+        return Response(serializer.data)
