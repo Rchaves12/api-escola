25013a4ab4918379b30c49660664ebacfb408e39
rafael.chaves <@Fonso0910>
2021-10-26 14:59:27 -0300

    metodo post

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -14,10 +15,22 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class AvaliacaoAPIView(APIView):
     """API Avaliação"""
     def get(self, request):
         avaliacao = Avaliacao.objects.all()
-        serializer = CursoSerializer(avaliacao, many=True)
+        serializer = AvaliacaoSerializer(avaliacao, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializers = AvaliacaoSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
