5568991866e48558abf2b1df112546e72bddee4f
rafael.chaves <@Fonso0910>
2021-10-26 16:11:15 -0300

    mudança para classe generica

--- cursos/views.py
+++ cursos/views.py
@@ -1,36 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """
-        API de Curso
-    """
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CursoAPIView(generics.ListAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
 
-class AvaliacaoAPIView(APIView):
-    """API Avaliação"""
-    def get(self, request):
-        avaliacao = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacao, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializers = AvaliacaoSerializer(data=request.data)
-        serializers.is_valid(raise_exception=True)
-        serializers.save()
-        return Response(serializers.data, status=status.HTTP_201_CREATED)
+class AvaliacaoAPIView(generics.ListAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
